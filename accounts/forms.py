a7d783ca234834bc95a10756d4ea00515bc2de5e
Jordan Tran <thai.jordan@gmail.com>
2022-10-24 19:31:49 -0700

    Feature 10 complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django import forms
 
 
 class LogInForm(forms.Form):
@@ -7,4 +6,12 @@ class LogInForm(forms.Form):
     password = forms.CharField(
         max_length=150,
         widget=forms.PasswordInput,
+    )
+
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(max_length=150, widget=forms.PasswordInput,)
+    password_confirmation = forms.CharField(
+        max_length=150, widget=forms.PasswordInput
     )
